d9b95a1baee61970bfc6e47ece3a5a0c25ece3d7
Matus <matusino.sykora@gmail.com>
2021-09-06 22:16:02 +0200

    Message model added

---
+++ src/main/java/com/sykora/chatapp/model/Message.java
@@ -0,0 +1,13 @@
+package com.sykora.chatapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    private String user;
+    private String message;
+}
